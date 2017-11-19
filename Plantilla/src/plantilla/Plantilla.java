b9ca42fbe823a9f2ee8ac1e5253668f06210f9c2
zachlagan <33263589+zachlagan@users.noreply.github.com>
2017-11-19 16:10:31 -0500

    Plantilla
    Ups

--- Plantilla/src/plantilla/Plantilla.java
+++ Plantilla/src/plantilla/Plantilla.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package plantilla;
-import tiquetes.*;
+import combos.ComboTemporada;
+import combos.Combo;
+import combos.ComboPareja;
+import combos.ComboDeluxe;
 import gestor.*;
 /**
  *
