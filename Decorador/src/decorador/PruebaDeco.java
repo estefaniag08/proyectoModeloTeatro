d8eabfc4cbc25bf57cb3ec8cda5c6a5952c9d34c
zachlagan <33263589+zachlagan@users.noreply.github.com>
2017-11-21 21:10:08 -0500

    Para que no digan que no soy aspero

--- Decorador/src/decorador/PruebaDeco.java
+++ Decorador/src/decorador/PruebaDeco.java
@@ -17,29 +17,23 @@ public class PruebaDeco {
 		salida.mostrar("2.preparar para cine 3d");
 		salida.mostrar("3.preparar para cine 4d");
 		salida.mostrar("4.preparar para cine 6d");
-		while (true) {
-			opcion = entrada.leerEntero();
-			switch (opcion) {
-			case 1:
-				salida.mostrar(sala.prepararFuncion());
-				break;
-			case 2:
-				dec = new Cine3d((Cine) sala);
-				salida.mostrar(dec.prepararFuncion());
-				break;
-			case 3:
-				dec = new Cine4d((Cine) sala);
-				salida.mostrar(dec.prepararFuncion());
-				break;
-			case 4:
-				dec = new Cine6d((Cine) sala);
-				salida.mostrar(dec.prepararFuncion());
-				break;
-			}
-
-			if (opcion == 0) {
-				break;
-			}
+		opcion = entrada.leerEntero();
+		switch (opcion) {
+		case 1:
+			salida.mostrar(sala.prepararFuncion());
+			break;
+		case 2:
+			dec = new Cine3d((Cine) sala);
+			salida.mostrar(dec.prepararFuncion());
+			break;
+		case 3:
+			dec = new Cine4d((Cine) sala);
+			salida.mostrar(dec.prepararFuncion());
+			break;
+		case 4:
+			dec = new Cine6d((Cine) sala);
+			salida.mostrar(dec.prepararFuncion());
+			break;
 		}
 	}
 }
